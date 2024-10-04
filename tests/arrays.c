c699a7c2b1374d2c295dc0e6f5362c8ec2332a11
mar <arch-legion>
2024-10-05 00:06:16 +0200

    update array test

--- tests/arrays.c
+++ tests/arrays.c
@@ -10,5 +10,19 @@ int main(int argc, char** argv) {
     void* x = sarr_get(list, 0);
     printf("%d %s\n", 0, (char*)x);
 
+    sarr* list2 = sarr_new();
+    int a = 1, b = 0, c = 2;
+    assert(list2);
+    res = sarr_add(list2, &a);
+    assert(res == 0);
+    res = sarr_add(list2, &b);
+    assert(res == 0);
+    res = sarr_add(list2, &c);
+    assert(res == 0);
+    for(size_t i = 0; i < list2->len; i++) {
+	x = sarr_get(list2, i);
+	printf("%d\n", *(int*)x);
+    }
+
     sarr_free(list);
 }
